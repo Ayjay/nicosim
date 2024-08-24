fa2dff0d4e934f35456bce0c4a546dd7deb35afa
Andrew Morris <andrewzmorris@gmail.com>
2024-08-24 02:14:06 +0000

    Refactor to clean things up

--- nicosim.py
+++ nicosim.py
@@ -1,4 +1,39 @@
-import sys
+from dataclasses import dataclass
+
+debug = False
+if debug:
+    debugprint = print
+else:
+    def debugprint(x): pass
+
+@dataclass
+class OutputBelt:
+    name: str
+    sink_rate: int
+    belt_rate: int
+    buffer_limit: int
+    item_entering: bool = False
+    buffer: int = 0
+    total_sunk: int = 0
+
+    def item_enter(self):
+        if self.item_entering:
+            debugprint(f'{self.name}: item enters')
+            self.item_entering = False
+            return True
+        return False
+    
+    def belt_move(self):
+        if not self.item_entering and self.buffer < self.buffer_limit:
+            debugprint(f'{self.name}: moves item to buffer')
+            self.buffer += 1
+            self.item_entering = False
+    
+    def sink(self):
+        if self.buffer > 0:
+            debugprint(f'{self.name}: item taken from buffer')
+            self.buffer -= 1
+            self.total_sunk += 1
 
 tick_rate = 30
 ticks_per_min = tick_rate * 60
@@ -7,48 +42,8 @@ input_rate = 390
 input_belt = 480
 input_buffer = 0
 
-debug = False
-if debug:
-    debugprint = print
-else:
-    def debugprint(x): pass
-
-class OutputBelt:
-    name = ""
-    sink_rate = 280
-    belt_rate = 480
-    entry = 0
-    buffer = 0
-    buffer_limit = 0
-    total = 0
-
-    def __init__(self,name,sink_rate,belt_rate,buffer_limit):
-        self.name = name
-        self.sink_rate = sink_rate
-        self.belt_rate = belt_rate
-        self.buffer_limit = buffer_limit
-    
-    def item_enter(self):
-        if self.entry == 0:
-            debugprint(f'{self.name}: item enters')
-            self.entry = 1
-            return True
-        return False
-    
-    def belt_move(self):
-        if self.entry == 1 and self.buffer < self.buffer_limit:
-            debugprint(f'{self.name}: moves item to buffer')
-            self.buffer += 1
-            self.entry = 0
-    
-    def sink(self):
-        if self.buffer > 0:
-            debugprint(f'{self.name}: item taken from buffer')
-            self.buffer -= 1
-            self.total += 1
-
-left = OutputBelt('left', 280, 480, 200)
-right = OutputBelt('right', 110, 120, 200000)
+left = OutputBelt(name='left', sink_rate=280, belt_rate=280, buffer_limit=200)
+right = OutputBelt(name='right', sink_rate=110, belt_rate=120, buffer_limit=200000)
 
 def timed_eventer(interval, action):
     time_interval = (ticks_per_min / interval) if interval > 0 else 0
@@ -73,7 +68,7 @@ def splitter():
 
 actions = [
     timed_eventer(input_rate, generate_input),
-    timed_eventer(0, splitter),
+    timed_eventer(input_belt, splitter),
     timed_eventer(left.belt_rate, left.belt_move),
     timed_eventer(left.sink_rate, left.sink),
     timed_eventer(right.belt_rate, right.belt_move),
@@ -88,5 +83,5 @@ for _ in range(ticks):
         next(action)
 
 print(f'input_buffer: {input_buffer}')
-print(f'left.buffer: {left.buffer} left.total {left.total}')
-print(f'right.buffer: {right.buffer} right.total {right.total}')
+print(f'left.buffer: {left.buffer} left.total {left.total_sunk}')
+print(f'right.buffer: {right.buffer} right.total {right.total_sunk}')
